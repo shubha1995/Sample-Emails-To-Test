06eafc3092a8ff74c146608ddb7cac8588d89665
shubhasankar <ssbhaumikdmr@gmail.com>
2021-07-07 08:21:28 +0530

    Added: tld cannot start with dot

--- src/com/email/Regex_email.java
+++ src/com/email/Regex_email.java
@@ -7,8 +7,8 @@ public class Regex_email {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Sample Email Test");
 		
-		String email = "abc@yahoo.com";
-		String regex1 = "^[a-zA-Z]+@[a-zA-Z]+[.][a-zA-Z]{2,3}$";
+		String email = "abc-100@yahoo.com";
+		String regex1 = "^[a-zA-Z]+([-][0-9A]+)*@[a-zA-Z]+[.][a-zA-Z]{2,3}$";
 			 
 			Pattern p = Pattern.compile(regex1);
 	        Matcher m = p.matcher(email);
