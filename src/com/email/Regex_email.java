00dd2c3a287641d60177a49fc54e521e0c7c7e22
shubhasankar <ssbhaumikdmr@gmail.com>
2021-07-07 08:16:24 +0530

    Added: Email must contains @ symbol

--- src/com/email/Regex_email.java
+++ src/com/email/Regex_email.java
@@ -1,8 +1,22 @@
 package com.email;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex_email {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Sample Email Test");
+		
+		String email = "abc@yahoo.com";
+		String regex1 = "^[a-zA-Z]+@[a-zA-Z]+[.][a-zA-Z]{2,3}$";
+			 
+			Pattern p = Pattern.compile(regex1);
+	        Matcher m = p.matcher(email);
+	        boolean r = m.matches();
+	        if (r)
+	        	System.out.println("Valid Email");
+	        else
+	        	System.out.println("Invalid Email");
 	}
 
 }
